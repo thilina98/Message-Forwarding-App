e885d9638406bd01a79612cc705421741427e095
Ravindu987 <ravindumadawalagama@gmail.com>
2022-05-27 11:56:31 +0530

    SMS listener set

--- app/src/main/java/com/example/smsforwardapp/MySmsReceiver.java
+++ app/src/main/java/com/example/smsforwardapp/MySmsReceiver.java
@@ -17,50 +17,50 @@ public class MySmsReceiver extends BroadcastReceiver {
 
     private TextView textView;
 
-//    @Override
-//    public void onReceive(Context context, Intent intent) {
-//        Toast.makeText(context,"Thilina Got the sms",Toast.LENGTH_SHORT).show();
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Thilina Got the sms",Toast.LENGTH_SHORT).show();
+
+        TextView textView;
+        String stringNumber = "+94710764814";
+
+        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), null,null, null, "date desc limit 1");
+        cursor.moveToFirst();
+        // you can't use getContentResolver() directly outside main activity class. but you can do it by using it as context.getContentResolver.
+        String stringMessage = cursor.getString(12);
+        SmsManager smsManagerSend = SmsManager.getDefault();
+        smsManagerSend.sendTextMessage(stringNumber, null, stringMessage, null, null);
+
+    }
+
+
+//        private SharedPreferences preferences;
 //
-//        TextView textView;
-//        String stringNumber = "+94710764814";
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
 //
-//        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), null,null, null, "date desc limit 1");
-//        cursor.moveToFirst();
-//        // you can't use getContentResolver() directly outside main activity class. but you can do it by using it as context.getContentResolver.
-//        String stringMessage = cursor.getString(12);
-//        SmsManager smsManagerSend = SmsManager.getDefault();
-//        smsManagerSend.sendTextMessage(stringNumber, null, stringMessage, null, null);
+//            Toast.makeText(context,"recieved",Toast.LENGTH_SHORT).show();
 //
-//    }
-
-
-        private SharedPreferences preferences;
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // TODO Auto-generated method stub
-            Toast.makeText(context,"Received",Toast.LENGTH_SHORT).show();
-
-            if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-                Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
-                SmsMessage[] msgs = null;
-                String msg_from;
-                if (bundle != null){
-                    //---retrieve the SMS message received---
-                    try{
-                        Object[] pdus = (Object[]) bundle.get("pdus");
-                        msgs = new SmsMessage[pdus.length];
-                        Toast.makeText(context,msgs[0].getMessageBody(),Toast.LENGTH_SHORT).show();
-                        for(int i=0; i<msgs.length; i++){
-                            msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-                            msg_from = msgs[i].getOriginatingAddress();
-                            String msgBody = msgs[i].getMessageBody();
-                        }
-                    }catch(Exception e){
-//                            Log.d("Exception caught",e.getMessage());
-                    }
-                }
-            }
-        }
+//            if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+//                Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+//                SmsMessage[] msgs = null;
+//                String msg_from;
+//                if (bundle != null){
+//                    //---retrieve the SMS message received---
+//                    try{
+//                        Object[] pdus = (Object[]) bundle.get("pdus");
+//                        msgs = new SmsMessage[pdus.length];
+//                        Toast.makeText(context,msgs[0].getMessageBody(),Toast.LENGTH_SHORT).show();
+//                        for(int i=0; i<msgs.length; i++){
+//                            msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+//                            msg_from = msgs[i].getOriginatingAddress();
+//                            String msgBody = msgs[i].getMessageBody();
+//                        }
+//                    }catch(Exception e){
+////                            Log.d("Exception caught",e.getMessage());
+//                    }
+//                }
+//            }
+//        }
     }
 
