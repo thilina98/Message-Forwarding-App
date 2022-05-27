8d70a427bf6992fcfd7667bdd1933e46ef153558
Ravindu987 <ravindumadawalagama@gmail.com>
2022-05-27 10:10:11 +0530

    sms receiver implemented reading inside receiving function is to be done

--- app/src/main/java/com/example/smsforwardapp/MySmsReceiver.java
+++ app/src/main/java/com/example/smsforwardapp/MySmsReceiver.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MySmsReceiver extends BroadcastReceiver {
 
+    private TextView textView;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"),null, null, null, null);
+        Toast.makeText(context,"Thilina Got the sms",Toast.LENGTH_SHORT).show();
+
+        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/inbox"), null,null, null, "date desc limit 1");
         // you can't use getContentResolver() directly outside main activity class. but you can do it by using it as context.getContentResolver.
+
 
 
     }
